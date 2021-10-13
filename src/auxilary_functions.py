a070c2e509b14b8b2ab3df6247d275bee3d4c73f
zhivkoplias <homo.korvin@gmail.com>
2021-10-14 00:21:16 +0200

    fig5C added

--- src/auxilary_functions.py
+++ src/auxilary_functions.py
@@ -582,7 +582,7 @@ def analyze_exctracted_network(cfg, path_to_tsv, network_label, network_rep, siz
         interaction_matrix = np.array(pd.read_csv(path_to_tsv, header = None, sep=','))
         interaction_matrix = np.apply_along_axis(list, 1, interaction_matrix)
         interaction_matrix = (interaction_matrix > 0).astype(np.int_)
-        print(interaction_matrix)
+        #print(interaction_matrix)
     else:
         edges = pd.read_csv(path_to_tsv, sep="\t")
         if network_label == 'gnw':
@@ -695,3 +695,48 @@ def create_nx_network(n_trials,sparsity,size,out_dir):
             writer.writerows(adj_list)
         
     return
+
+def analyze_connectivity(path_to_tsv, network_label, network_rep, size):
+    """
+    collect topological stats from extracted networks
+    """
+    import networkx as nx
+    if network_label == 'randg' or network_label == 'dag':
+        interaction_matrix = np.array(pd.read_csv(path_to_tsv, header = None, sep=','))
+        interaction_matrix = np.apply_along_axis(list, 1, interaction_matrix)
+        interaction_matrix = (interaction_matrix > 0).astype(np.int_)
+        #print(interaction_matrix)
+    else:
+        edges = pd.read_csv(path_to_tsv, sep="\t")
+        if network_label == 'gnw':
+            edges.columns = ["tf", "tg", "strength"]
+            edges = edges[["tf", "tg"]]
+        else:
+            edges.columns = ["tf", "tg"]
+        edges['tf'].astype(str)
+        edges['tg'].astype(str)
+        edges.columns = ["tf", "tg"]
+    
+        nodes = sorted(np.unique(np.concatenate((edges.tf.unique(), edges.tg.unique()))))
+        nodes = pd.DataFrame(data=range(len(nodes)), index=nodes, columns=["idx"])
+        edges_ = edges.join(nodes, on="tf").join(nodes, on="tg", lsuffix="_tf", rsuffix="_tg")
+        np_edges = edges_[["idx_tg", "idx_tf"]].values
+        interaction_matrix = build_Tnet(np_edges, len(nodes))
+    
+    interaction_matrix_graph = nx.DiGraph(interaction_matrix)
+    degree_list_test = sorted([d for n, d in interaction_matrix_graph.degree])
+
+    connectivity = {}
+    for number, edges in enumerate(degree_list_test):
+        av_nodes = sum(degree_list_test[number::])/len((degree_list_test[number::]))
+        frequency = np.round(len((degree_list_test[number::]))/len((degree_list_test)), 4)
+        connectivity[frequency] = np.round(np.log10(av_nodes), 4)
+    df_connectivity = pd.DataFrame({'frequency': list(connectivity.keys()),'average degree':list(connectivity.values())})
+    d = np.polyfit(df_connectivity['average degree'],df_connectivity['frequency'],1)
+    f = np.poly1d(d)
+    df_connectivity.insert(2,'Rfreq',f(df_connectivity['average degree']))
+    df_connectivity['network'] = network_label
+    df_connectivity['size'] = size
+    df_connectivity['rep'] = network_rep
+
+    return df_connectivity
