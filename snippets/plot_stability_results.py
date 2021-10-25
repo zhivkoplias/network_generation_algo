6adea5f60ef086b63853bbcc34ea7d05fdb6ee26
zhivkoplias <homo.korvin@gmail.com>
2021-10-25 11:16:59 +0200

    check

--- snippets/plot_stability_results.py
+++ snippets/plot_stability_results.py
@@ -8,18 +8,30 @@ import seaborn as sns
 import pandas as pd
 import numpy as np 
 import sys
+import os
 
-input = sys.argv[1]
 
-df = pd.read_csv(input,sep=",")
+plt.rcParams.update({'font.size': 26})
+
+file_path = '/home/erik/sweden/sonnhammer/GeneSnake/generation/network_generation_algo/results/tables/2021-10-06_stability_analysis_itter10_datasets10_4method_4sizes.csv'
+
+df = pd.read_csv(file_path,sep=",")
 
 df["log lambda"] = np.log10(abs(df["min jtlide_lambda"].values))
 
-sns.lineplot(x="size", y="log lambda",hue="Method",data=df)
-plt.ylabel("log10(abs(jtilde_lambda))")
-plt.xlabel("Size")
+fig, ax = plt.subplots(figsize=(16,12))
 
-if len(sys.argv) > 2:
-    plt.savefig(sys.argv[2])
-else:
-    plt.show()
+sns.lineplot(x="size", y="log lambda", markers=True, dashes=False,hue="Method",data=df,\
+             hue_order = ["randG", 'gnw', 'nx', "FFL", 'DAG'], ci=95, linewidth = 3)
+    
+ax.legend().set_title('Network')
+
+plt.ylabel(r'$\min$''$(\log10$''($\lambda$))')
+plt.xlabel("Size")
+plt.legend(loc=2)
+
+os.chdir('../results/figures')
+plt.savefig("figure5A.svg")
+plt.savefig("figure5A.png")
+
+plt.show()
